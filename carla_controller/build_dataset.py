a2d87c0a92121bbe028642a1a6d4d5f306855b92
Keith Chester <kchester@gmail.com>
2022-11-02 18:01:17 -0700

    chore: rename variable to include units

--- carla_controller/build_dataset.py
+++ carla_controller/build_dataset.py
@@ -3,7 +3,7 @@ from simulation import Simulation
 
 from random import randrange
 
-TIME_FOR_RUN = 60
+TIME_FOR_RUN_SECONDS = 60
 NUMBER_OF_RUNS = 100
 MIN_NUMBER_VEHICLES = 30
 MAX_NUMBER_VEHICLES = 200
@@ -11,11 +11,11 @@ MIN_NUMBER_PEDESTRIANS = 20
 MAX_NUMBER_PEDESTRIANS = 100
 
 def single_run() -> str:
-    number_of_vehicles = randrange(MIN_NUMBER_VEHICLES, MAX_NUMBER_VEHICLES + 1)
+    number_sof_vehicles = randrange(MIN_NUMBER_VEHICLES, MAX_NUMBER_VEHICLES + 1)
     number_of_pedestrians = randrange(MIN_NUMBER_PEDESTRIANS, MAX_NUMBER_PEDESTRIANS + 1)
 
     simulation = Simulation(vehicles=number_of_vehicles, pedestrians=number_of_pedestrians)
-    simulation.launch(TIME_FOR_RUN)
+    simulation.launch(TIME_FOR_RUN_SECONDS)
 
     return simulation.run_id
 
