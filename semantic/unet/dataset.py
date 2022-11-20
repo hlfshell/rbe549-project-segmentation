a46d5cad9cf64ed3bbd121dfbdfbe92df17ff6da
Keith Chester <kchester@gmail.com>
2022-11-19 19:31:06 -0800

    chore: Update imports from semantic move

--- semantic/unet/dataset.py
+++ semantic/unet/dataset.py
@@ -1,4 +1,4 @@
-from carla_controller.labels import CARLA_SEMANTIC_CATEGORY_MAPPINGS
+from semantic.carla_controller.labels import CARLA_SEMANTIC_CATEGORY_MAPPINGS
 
 import keras
 import numpy as np
