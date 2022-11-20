08efea7a068bd3ebcc3a4d58853884620d9b0d6d
Keith Chester <kchester@gmail.com>
2022-11-19 19:39:33 -0800

    chore: Moved trainer to better place/name

--- semantic/train_unet.py
+++ semantic/train_unet.py
@@ -1,3 +1,6 @@
+from semantic.carla_controller.labels import SEMANTIC_CATEGORIES
+from semantic.unet.model import get_unet
+
 from datetime import datetime
 from model import get_unet
 from train import train_unet
@@ -5,7 +8,7 @@ from train import train_unet
 
 img_size = (256, 256)
 
-model = get_unet(img_size, 13)
+model = get_unet(img_size, len(SEMANTIC_CATEGORIES))
 model = train_unet(model, 50, 32, img_size, test_size=0.25, dataset_folder="./dataset")
 
 model.save(f"./unet_model_{datetime.utcnow()}")
