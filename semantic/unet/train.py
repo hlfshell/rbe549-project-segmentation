60ef99b8f9324f6cd9ba6a3e57d5111f171f7821
Keith Chester <kchester@gmail.com>
2022-12-08 19:12:14 -0800

    feature: new focal loss usage in train.py w/ weights

--- semantic/unet/train.py
+++ semantic/unet/train.py
@@ -1,12 +1,18 @@
-import keras
-import os
-
 from semantic.unet.dataset import Carla
 
+import keras
+import math
+import os
+
+from focal_loss import SparseCategoricalFocalLoss
 from sklearn.model_selection import train_test_split
 from typing import Optional, Tuple
+from keras.losses import SparseCategoricalCrossentropy
 
-from keras.models import load_model
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
 
 def train_unet(
         model,
@@ -47,10 +53,30 @@ def train_unet(
     training_generator = Carla(batch_size, img_size, train_rgb_paths, train_label_paths)
     validation_generator = Carla(batch_size, img_size, validation_rgb_paths, validation_label_paths, data_augmentation=False)
 
-    # Configure the model for training.
-    # We use the "sparse" version of categorical_crossentropy
-    # because our target data is integers.
-    model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
+    # Below is a weight calculation built around:
+    # (# of pixels in average in a category per image) /
+    # (# of pixels in average for the given category per image)
+    class_weight=[
+            0.2314920504970292,
+            64.11203165414611,
+            0.4338712221910821,
+            9.73727668152528,
+            2.421319361944825,
+            2.8451573153682137,
+            2.0520724385563724,
+            189.19925153003993,
+        ]
+
+    # We use a sigmoid to compress our given class weights to a
+    # 0-1 range. We multiply this by 2 to increase our range to
+    # 0-2.
+    class_weight= [(2*sigmoid(x)) for x in class_weight]
+
+    model.compile(optimizer='rmsprop',
+    loss=SparseCategoricalFocalLoss(
+        2.0,
+        class_weight=class_weight
+    ))
 
     callbacks = [
         keras.callbacks.ModelCheckpoint(f"{checkpoint_directory}/unet.h5", save_best_only=True),
